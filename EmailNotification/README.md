aa599e97dfba65d958dd4b7cce1933a12dd82b6f
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2025-06-07 14:02:34 +0530

    Inital setup for Email Notification microservice

---
+++ EmailNotification/README.md
@@ -0,0 +1,3 @@
+## Email Notification 
+
+- This email notification microservice will act as consumer for the product microservice
---
+++ EmailNotification/src/test/java/io/projects/kafka/EmailNotification/EmailNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projects.kafka.EmailNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
